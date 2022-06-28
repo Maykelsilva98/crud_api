c9b029d83ed9bb4b9df3f5f2f6ece1409a044b99
diuh12 <diuhsousa@gmail.com>
2022-06-28 17:06:45 -0300

    feat: adicionado o CRUD e simulador de bd

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { bd } from "./infra/bd.js";
+import { produtos } from "./controllers/produtos_controller.js";
+
+const app = express();
+app.use(express.json());
+
+produtos(app, bd);
+
+app.listen(3003, () => {
+    console.log("TUDO OK!");
+  });
---
+++ src/infra/bd.js
@@ -0,0 +1,5 @@
+const bd = {
+    "produtos": []
+};
+
+export {bd};
