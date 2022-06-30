14116279d3390c2390673247aacd83e75d03e93a
Maykelsilva98 <mayckelsantos5@gmail.com>
2022-06-30 08:46:37 -0300

    fix: corrigindo alguns erros de sintaxe

--- src/controllers/pagamentosController.js
+++ src/controllers/pagamentosController.js
@@ -1,15 +1,16 @@
 import { Pagamentos } from "../models/pagamentosModel.js";
 
 export const pagamentosController = (app, bd) =>{
-    app.get("/pagementos", (req, res) =>{
+    app.get("/pagamentos", (req, res) =>{
         res.send(bd.pagamentos)
     })
 
     app.post("/pagamentos", (req, res) =>{
         const body = req.body
-        const novoPagamento = new Pagamentos(body.id,body.cliente_id, body.valor, body.data, 
+        const novoPagamento = new Pagamentos(body.id, body.cliente_id, body.valor, body.data, 
             body.forma_pagamento, body.id_compras )
         bd.pagamentos.push(novoPagamento)
+        res.send("Objeto adicionado")
     })
 
     app.delete("/pagamentos/:id", (req, res)=>{
@@ -28,7 +29,7 @@ export const pagamentosController = (app, bd) =>{
         const indice = bd.pagamentos.findIndex(x => x.id == par)
         const dadoAntigo = bd.pagamentos[indice]
         const dadoNovo = req.body
-
+        
         const pagamentoNovo = new Pagamentos(
             dadoNovo.id || dadoAntigo.id,
             dadoNovo.cliente_id || dadoAntigo.cliente_id,
@@ -36,9 +37,9 @@ export const pagamentosController = (app, bd) =>{
             dadoNovo.data || dadoAntigo.data,
             dadoNovo.forma_pagamento || dadoAntigo.forma_pagamento,
             dadoNovo.id_compras || dadoAntigo.id_compras
-        )
-
-        bd.pagamento.splice(indice, 1, pagamentoNovo)
+            )
+            
+        bd.pagamentos.splice(indice, 1, pagamentoNovo)
         res.json({"Pagamento antigo": dadoAntigo, "Pagamento novo": dadoNovo})
     })
 }
--- src/models/pagamentosModel.js
+++ src/models/pagamentosModel.js
@@ -1,7 +1,7 @@
 export class Pagamentos{
     constructor(id, cliente_id, valor, data, forma_pagamento, id_compras){
         this.id = id,
-        this.cliente_id = clientes_id,
+        this.cliente_id = cliente_id,
         this.valor = valor,
         this.data = data,
         this.forma_pagamento = forma_pagamento,
