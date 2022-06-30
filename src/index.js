cc8fafb20506c1dadd8af82d24dee6bf04a3429b
machadogadiel <github@gadiel.dev>
2022-06-29 22:17:42 -0300

    feat: implementados o CRUD, MVC e DB

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import chalk from 'chalk';
+import express from 'express';
+import { db } from './infra/db.js';
+import { ClientesController } from './controllers/ClientesController.js';
+
+const app = express()
+const port = 3000
+const address = `http://localhost:${port}`
+
+app.use(express.json())
+
+ClientesController(app, db)
+
+app.listen(port, () => {
+    console.log(chalk.cyan(`Listening to ${address}`))
+})
