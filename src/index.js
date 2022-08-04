456ff523832f564074b822236454669c4e81e65d
Maykelsilva98 <mayckelsantos5@gmail.com>
2022-08-03 21:32:46 -0300

    fix: corrigindo o index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import {bd_mysql as bd} from './infra/bd_mysql.js'
-import { ClientesController } from './controllers/ClientesController.js';
-import { pagamentosController } from './controllers/PagamentosController.js';
-import { compras } from './controllers/compras-controller.js';
+// import { ClientesController } from './controllers/ClientesController.js';
+// import { pagamentosController } from './controllers/PagamentosController.js';
+// import { compras } from './controllers/compras-controller.js';
 import { produtos } from './controllers/produtos_controller.js';
-import { ProdutosDao } from './DAO/produtos_dao.js'
+
 
 
 
